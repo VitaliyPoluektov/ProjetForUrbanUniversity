a9ac69fe5cc887db05495525cb6da08e3f7e4fab
VitaliyPoluektov <vitya78@mail.ru>
2024-11-30 10:07:52 +0500

    Add files via upload

--- module_3_2_rassilka_pisem.py
+++ module_3_2_rassilka_pisem.py
@@ -1,17 +1,15 @@
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
-    b = True
     if recipient.find('@') == -1 or sender.find('@') == -1 or (recipient[len(recipient)-4:] != '.com'\
             and recipient[len(recipient)-4:] != '.net' and recipient[len(recipient)-3:] != '.ru')\
             or (sender[len(sender)-4:] != '.net' and sender[len(sender)-3:] != '.ru' and sender[len(sender)-4:] != '.com'):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-        b = False
+        return
     if recipient == sender:
         print('Нельзя отправить письмо самому себе!')
-        b = False
+        return
     if sender == 'university.help@gmail.com':
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
-        b = False
-    if b:
+    else:
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
 
 
