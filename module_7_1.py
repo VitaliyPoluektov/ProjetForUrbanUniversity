8ddf640ec188546da1db26a6ee7af3050737334e
VitaliyPoluektov <vitya78@mail.ru>
2025-01-28 17:06:35 +0500

    Add files via upload
    Если вы решали старую версию задачи, проверка будет производиться по ней.
    
    Ссылка на старую версию тут.
    
    
    
    Цель: закрепить знания о работе с файлами (чтение/запись) решив задачу.
    
    
    
    Задача "Учёт товаров":
    
    Необходимо реализовать 2 класса Product и Shop, с помощью которых будет производиться запись в файл с продуктами.
    
    Объекты класса Product будут создаваться следующим образом - Product('Potato', 50.0, 'Vagetables') и обладать следующими свойствами:
    
    Атрибут name - название продукта (строка).
    Атрибут weight - общий вес товара (дробное число) (5.4, 52.8 и т.п.).
    Атрибут category - категория товара (строка).
    Метод __str__, который возвращает строку в формате '<название>, <вес>, <категория>'. Все данные в строке разделены запятой с пробелами.
    
    
    Объекты класса Shop будут создаваться следующим образом - Shop() и обладать следующими свойствами:
    
    Инкапсулированный атрибут __file_name = 'products.txt'.
    Метод get_products(self), который считывает всю информацию из файла __file_name, закрывает его и возвращает единую строку со всеми товарами из файла __file_name.
    Метод add(self, *products), который принимает неограниченное количество объектов класса Product. Добавляет в файл __file_name каждый продукт из products, если его ещё нет в файле (по полю name И по полю category). Если такой продукт уже есть, то увеличивает общий вес и выводит строку 'Продукт <название> уже был в магазине, его общий вес теперь равен <вес> .
    
    
    Пример результата выполнения программы:
    
    Пример работы программы:
    
    s1 = Shop()
    
    p1 = Product('Potato', 50.5, 'Vegetables')
    
    p2 = Product('Spaghetti', 3.4, 'Groceries')
    
    p3 = Product('Potato', 5.5, 'Vegetables')
    
    
    
    s1.add(p1, p2, p3)
    
    
    
    print(s1.get_products())

--- module_7_1.py
+++ module_7_1.py
@@ -11,8 +11,8 @@ class Product:
 class Shop:
     __file_name = 'products.txt'
     def __init__(self):
-        self.__file_name = open(self.__file_name, 'a')
-        self.__file_name.close()
+        file = open(self.__file_name, 'a')
+        file.close()
 
     def get_products(self):
         file = open(self.__file_name, 'r')
@@ -21,12 +21,31 @@ class Shop:
         return name_prod
 
     def add(self, *products):
+        product = self.get_products()
         existing_products = {}
-        file = open(self.__file_name, 'r')
-        for i in file:
-            name, weight, category = i.strip().split(', ')
-            existing_products[(name, category)] = float(weight)
+        if product != '':
+            productss = product.strip().split('\n')
+            for l in productss:
+                name, weight, category = l.strip().split(', ')
+                existing_products[(name, category)] = float(weight)
+        file = open(self.__file_name, 'a')
+        for p in products:
+            key = (p.name, p.category)
+            if key in existing_products:
+                    existing_products[key] += p.weight
+                    print(
+                        f'Продукт {p.name} уже был в магазине, его общий вес теперь'
+                        f' равен {existing_products[key]}.')
+            else:
+                    existing_products[key] = p.weight
+            file.write(f'{p.name}, {existing_products[key]}, {p.category}\n')
         file.close()
 
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
 
+s1.add(p1, p2, p3)
 
+print(s1.get_products())
